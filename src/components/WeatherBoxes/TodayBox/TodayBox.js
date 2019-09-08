15142956f50b2afef4c1b4ac96a976cba7c553fa
omerme13 <omerme13@gmail.com>
2019-09-08 22:11:50 +0300

    bug fixed

--- src/components/WeatherBoxes/TodayBox/TodayBox.js
+++ src/components/WeatherBoxes/TodayBox/TodayBox.js
@@ -6,9 +6,9 @@ import './TodayBox.scss';
 const todayBox = props => {
     return (
         <div className="today-box">
-            <GraphButton clicked={props.clicked} />
             <div className="today-box__location">{`${props.city}, ${props.country}`}</div>
             <div className="today-box__day">today</div>
+            <GraphButton clicked={props.clicked} />
             <div className="today-box__degrees">{props.deg}&deg;</div>
             <div className="today-box__cond">{props.cond}</div>
             <div className="today-box__humidity">{`humidity: ${props.humidity}%`}</div>
